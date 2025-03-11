1338cf1a27deba4a2ccf6fe4ac7c2717d4b90dfb
yuqi.wang <yukiontheiceberg@gmail.com>
2025-03-11 22:30:37 +0000

    update models

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import subprocess
 
 # LIMO
 # MODEL_NAME_OR_PATH = "/mbz/users/yuqi.wang/LIMO/train/saves/qwen2.5-32b-instruct/full/limo_sft/checkpoint-390"
+# MODEL_NAME_OR_PATH = "GAIR/LIMO"
 # IQ
 # MODEL_NAME_OR_PATH = "/mbz/users/yuqi.wang/LLaMA-Factory/saves/qwen2.5-32b-instruct/full/iq_sft/checkpoint-1000/"
 # COLDSTART
 # MODEL_NAME_OR_PATH = "/mbz/users/richard.fan/LLaMA-Factory/qwen32b_instruct_coldstart"
 # MODEL_NAME_OR_PATH = "/mbz/users/richard.fan/LLaMA-Factory/saves/qwen2.5-32b-instruct-5epochs/full/sft/checkpoint-80"
+# MODEL_NAME_OR_PATH = "/mbz/users/yuqi.wang/RL-eval/models/coldstart-2"
+# MODEL_NAME_OR_PATH = "/mbz/users/yuqi.wang/RL-eval/models/coldstart-3"
 # MODEL_NAME_OR_PATH = "/mbz/users/richard.fan/LLaMA-Factory/saves/qwen2.5-32b-instruct-16-gpu-5epochs/full/sft/checkpoint-315"
 # QWEN2.5-32B
 # MODEL_NAME_OR_PATH = "Qwen/Qwen2.5-32B"
 # QWEN2.5-32B-instruct
 # MODEL_NAME_OR_PATH =  "Qwen/Qwen2.5-32B-instruct"
 # DEEPSEEK-R1-DISTILL-QWEN-32B
-MODEL_NAME_OR_PATH =  "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B"
-# INTERNAL_RL
-# MODEL_NAME_OR_PATH = "/mbz/users/shibo.hao/Reasoning360/checkpoints/Reasoning360/shibo-math-grpo-32nodes-setting2-Qwen2.5-32B/global_step_480/actor/huggingface"
+# MODEL_NAME_OR_PATH =  "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B"
+# LLAMA-3.3-70B-instruct
+# MODEL_NAME_OR_PATH = "meta-llama/Llama-3.3-70B-Instruct"
+# MODEL_NAME_OR_PATH = "Qwen/QwQ-32B-Preview"
+# INTERNAL_RL (n = 48, 96, 144, 192, 240, 288)
+MODEL_NAME_OR_PATH = "/mbz/users/shibo.hao/Reasoning360/checkpoints/Reasoning360/shibo-math-grpo-32nodes-setting2-Qwen2.5-32B/global_step_288/actor/huggingface"
 
-# BENCHMARKS_TO_RUN = ["aime24", "math", "gpqa", "mmlu_pro", "mmlu", "ifeval"]
-BENCHMARKS_TO_RUN = ["aime24", "math"]
+# BENCHMARKS_TO_RUN = ["aime24", "math", "gpqa_diamond", "mmlu_pro", "mmlu", "ifeval"]
+BENCHMARKS_TO_RUN = ["gpqa_diamond"]
 
 SUPPORTED_BENCHMARKS = {
     "aime24": {
@@ -30,8 +36,16 @@ SUPPORTED_BENCHMARKS = {
         "n_fewshot": 0,
         "n_sampling": 1
     },
+    "gpqa_diamond": {
+        "n_fewshot": 0,
+        "n_sampling": 1
+    },
+    "gpqa_main": {
+        "n_fewshot": 0,
+        "n_sampling": 1
+    },
     "gpqa": {
-        "n_fewshot": 5,
+        "n_fewshot": 0,
         "n_sampling": 1
     },
     "ifeval": {
@@ -39,7 +53,7 @@ SUPPORTED_BENCHMARKS = {
         "n_sampling": 1
     },
     "mmlu": {
-        "n_fewshot": 5,
+        "n_fewshot": 0,
         "n_sampling": 1
     },
     "mmlu_pro": {
@@ -74,11 +88,11 @@ for benchmark in BENCHMARKS_TO_RUN:
         print("=" * 50)
         continue
     # supported by qwen2.5-math
-    if benchmark in ["aime24", "math", "gpqa"]:
+    if benchmark in ["aime24", "math", "gpqa_diamond", "gpqa_main"]:
         # prompt_type = SUPPORTED_TEMPLATES.get(MODEL_NAME_OR_PATH, "internal-rl")
         print(f"qwen2.5-math running target benchmark: {benchmark}...")
         print("=" * 50)
-        subprocess.run([
+        completed_process = subprocess.run([
             "sbatch",
             "./sbatch_scripts/qwen2.5-math.sh",
             # ./qwen2.5-math/evaluation/utils.py
@@ -89,7 +103,7 @@ for benchmark in BENCHMARKS_TO_RUN:
             str(SUPPORTED_BENCHMARKS[benchmark]["n_fewshot"]),
             str(SUPPORTED_BENCHMARKS[benchmark]["n_sampling"]),
             # max tokens per call
-            "32768",
+            "16384",
             str(TEMPERATURE),
             os.path.abspath(OUTPUT_DIR)
         ], check=True)
@@ -97,7 +111,7 @@ for benchmark in BENCHMARKS_TO_RUN:
     else:
         print(f"lm-evalulation-harness running target benchmark: {benchmark}...")
         print("=" * 50)
-        subprocess.run([
+        completed_process = subprocess.run([
             "sbatch",
             "./sbatch_scripts/harness.sh",
             MODEL_NAME_OR_PATH,
@@ -106,3 +120,4 @@ for benchmark in BENCHMARKS_TO_RUN:
             f"temperature={TEMPERATURE}",
             OUTPUT_DIR
         ], check=True)
+    print(completed_process.args)
