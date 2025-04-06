47706b21cc8d0e458ad2aeef66975c407246d4d9
realYuanLi <yl967@cam.ac.uk>
2025-04-06 13:13:07 +0000

    add evalplus

---
+++ code_eval/eval_plus/test_humaneval.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#SBATCH --partition=mbzuai
+#SBATCH --time=100:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:8
+#SBATCH --job-name=lm_eval
+
+# Load necessary modules and environment
+module load cuda/12.4
+source ~/miniconda3/bin/activate evalplus-eval
+
+
+# Set NCCL to debug only warnings
+export NCCL_DEBUG=WARN
+
+# Disable NVML if not required and set additional env variables
+export PYTORCH_NO_NVML=1
+export HF_ALLOW_CODE_EVAL=1
+export CUDA_LAUNCH_BLOCKING=1
+
+# Set additional environment variables
+export HF_DATASETS_OFFLINE=1
+export HF_EVALUATE_OFFLINE=1
+
+export PATH=./vllm/bin:$PATH
+export PYTHONPATH=$PYTHONPATH:./eval_plus/evalplus
+
+current_time=$(date +%Y%m%d_%H%M%S)
+
+
+ROOT_DIR="$1"
+# Accept the model path as the second argument; this should be an absolute path.
+MODEL_DIR="$2"
+MODEL="$3"
+
+# Create output directories relative to the ROOT_DIR
+mkdir -p "${ROOT_DIR}/printout/output_file"
+mkdir -p "${ROOT_DIR}/printout/error_file"
+TP=4
+
+exec >> "${ROOT_DIR}/printout/output_file/output_humaneval_${SLURM_JOB_ID}_${current_time}.out" 2>> "${ROOT_DIR}/printout/error_file/error_humaneval_${SLURM_JOB_ID}_${current_time}.err"
+
+mkdir -p ${ROOT_DIR}/humaneval/${MODEL}/response
+
+echo "EvalPlus: ${MODEL_DIR}, ROOT_DIR ${ROOT_DIR}"
+
+python generate.py \
+  --model_type qwen2 \
+  --model_size chat \
+  --model_path ${MODEL_DIR} \
+  --bs 16 \
+  --temperature 0.6 \
+  --n_samples 1 \
+  --greedy \
+  --root  ${ROOT_DIR}/humaneval/${MODEL}/response \
+  --dataset humaneval \
+  --tensor-parallel-size ${TP}
+
+python -m evalplus.sanitize --samples ${ROOT_DIR}/humaneval/${MODEL}/response
+
+evalplus.evaluate \
+  --dataset humaneval \
+  --samples ${ROOT_DIR}/humaneval/${MODEL}/response > ${ROOT_DIR}/humaneval/${MODEL}/raw_humaneval_results.txt
+
+evalplus.evaluate \
+  --dataset humaneval \
+  --samples ${ROOT_DIR}/humaneval/${MODEL}/response-sanitized > ${ROOT_DIR}/humaneval/${MODEL}/humaneval_results.txt
